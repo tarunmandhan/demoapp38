63ed699eababe243839a0be833a917d1444ab826
Tarun Mandhan <mandhantarun@gmail.com>
2022-12-29 16:40:44 +0530

    Now alert and incease+1 working using useEffect 😀

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
+  useEffect(() => {
+    alert("I am clicked");
+  });
 
   return (
     <>
       <button
-        onClick={
-          (() => {
-            setNum(num + 1);
-          },
-          () => {
-            alert("I am clicked");
-          })
-        }
+        onClick={() => {
+          setNum(num + 1);
+        }}
       >
         Click me {num}
       </button>
@@ -21,3 +19,5 @@ const App = () => {
   );
 };
 export default App;
+
+// at the time add alert the setNum(num+1) is not working. Now we use useEffect (1. useEffect always expect a function
