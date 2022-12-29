0c345c5b85e43b06a2c561f0eb31c086221b4801
Tarun Mandhan <mandhantarun@gmail.com>
2022-12-29 17:16:17 +0530

    1st btn every click msg & 2nd btn only reload msg

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
+  const [nums, setNums] = useState(0);
   useEffect(() => {
     alert("I am clicked");
-  });
+    console.log("i am ok");
+  }, [num]); // the empty array is use for only show alert msg at only first time loading page or refresh page.after that no alert msg.
+  // but in empty array we pass num or nums then only show msg at first click then no msg again show.
 
   return (
     <>
@@ -15,9 +18,20 @@ const App = () => {
       >
         Click me {num}
       </button>
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setNums(nums + 1);
+        }}
+      >
+        Click me {nums}
+      </button>
     </>
   );
 };
 export default App;
 
-// at the time add alert the setNum(num+1) is not working. Now we use useEffect (1. useEffect always expect a function
+// at the time add alert the setNum(num+1) is not working.
+// Now we use useEffect (1. useEffect always expect a function) and both are working now but every click has a alert.
+//
